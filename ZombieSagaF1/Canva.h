5cbefa09371c7071d031e38894ce8fc27c1175a1
hidingcode <yclim4635@gmail.com>
2022-09-23 18:55:39 +0800

    revert change

--- ZombieSagaF1/Canva.h
+++ ZombieSagaF1/Canva.h
@@ -3,11 +3,7 @@
 #include "Image.h"
 #include "VerticalPanel.h"
 #include "Button.h"
-#include "Text.h"
-#include <vector>
-using namespace std;
 
-// Testing stuff
 class Canva: public Image
 {
 public:
@@ -17,13 +13,11 @@ public:
 	void CreateTexture(IDirect3DDevice9* d3dDevice, LPCSTR textureFilePath);
 	void Render(LPD3DXSPRITE spriteBrush, D3DXMATRIX* mat);
 	void OnCollide(RECT Colrect);
-	void AddNewText(Text text);
-	void AddNewVPanel(VerticalPanel vPanel);
-	void AddNewButton(Button button);
+
 private:
-	vector<Text*> textList;
-	vector<VerticalPanel*> vPanelList;
-	vector<Button*> buttonList;
+	//VerticalPanel vPanel[];
+	//Button button[];
+	//D3DXVECTOR2(310, 300)
 };
 #endif
 
